ddb5a3beb47f64628217679339fb69f7f132bc52
Simone F <groppo8@gmail.com>
2014-02-22 11:15:49 +0100

    Fix check_postgresql_running
    Skip this check if "/etc/init.d/postgresql" does not exist.
    Bug reported by Daniele Forsi

--- utils.py
+++ utils.py
@@ -8,10 +8,12 @@ from subprocess import Popen, PIPE
 
 def check_postgresql_running():
     #Check if PostgreSQL server is running
-    status = Popen(['/etc/init.d/postgresql', 'status'], stdout=PIPE, stderr=PIPE)
-    (stdoutdata, err) = status.communicate()
-    if stdoutdata[-5:-1] == 'down':
-        sys.exit("\nPlease, start PostgreSQL server.")
+    fileName = "/etc/init.d/postgresql"
+    if os.path.exists(fileName):
+        status = Popen([fileName, 'status'], stdout=PIPE, stderr=PIPE)
+        (stdoutdata, err) = status.communicate()
+        if stdoutdata[-5:-1] == 'down':
+            sys.exit("\nPlease, start PostgreSQL server.")
 
 
 def make_dir(path):
